acea0d499128a1cbce22a9f8c3a428c8cf5fc314
JoHeeGun <whgmlrjs123@naver.com>
2022-12-13 13:32:27 +0900

    카카오 api 호출 연동

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,39 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  mounted() {
+    this.searchBook()
+  },
+  methods:{
+    async searchBook() {
+      try {
+        let sort = '';
+        let target = '';
+        let title = '';            
+
+        let api = await axios.get(
+          `https://dapi.kakao.com/v3/search/book?sort=${sort}&target=${target}&page=1`,
+          {
+            headers: {
+              Authorization: 'KakaoAK 3b9b7e5018ae18db72532378d670854e',
+            },
+            params: {
+              query: title,
+            },
+          },
+        );
+
+        console.log('api',api)
+
+      } catch (err) {
+        console.log(err);
+      }
+      
+    }
+  }
 }
 </script>
 
